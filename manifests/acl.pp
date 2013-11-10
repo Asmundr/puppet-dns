3a6ff83777b289f7f74f6a492326298e40505ca8
Asmundr <asmundr@torpnet.us>
2013-11-09 23:10:13 -0500

    Changed single quotes to double

--- manifests/acl.pp
+++ manifests/acl.pp
@@ -25,7 +25,7 @@ define dns::acl (
 
   concat::fragment { "named.conf.local.acl.${name}.include":
     ensure  => $ensure,
-    target  => '${dns::server::params::cfg_dir}/named.conf.local',
+    target  => "${dns::server::params::cfg_dir}/named.conf.local",
     order   => 2,
     content => template("${module_name}/acl.erb"),
   }
